d9ca567ee015c1b306162f2b7458161c62785210
dev-ebruates <devatesebru@gmail.com>
2024-12-08 22:31:21 +0300

    Post gönderilirken oluşturulma zamanına göre sıralandı.

--- Application/Features/Posts/GetAllPostCommand.cs
+++ Application/Features/Posts/GetAllPostCommand.cs
@@ -19,7 +19,7 @@ public class GetAllPostCommand : IRequest<Response<List<GetAllPostCommandRespons
         var posts = await unitOfWork.PostRepository.GetAll();
         if (posts == null || posts.Count == 0)
           return Response<List<GetAllPostCommandResponse>>.CreateSuccessResponse(new List<GetAllPostCommandResponse>(0), "No posts found");
-        var postResponses = posts.Select(post => new GetAllPostCommandResponse(post.Id, post.User.Username, post.User.ProfilePicture, post.MediaUrl, post.Caption)).ToList();
+        var postResponses = posts.Select(post => new GetAllPostCommandResponse(post.Id, post.User.Username, post.User.ProfilePicture, post.MediaUrl, post.Caption, post.CreatedAt)).OrderByDescending(x => x.CreatedAt).ToList();
         return Response<List<GetAllPostCommandResponse>>.CreateSuccessResponse(postResponses);
       }
       catch (Exception)
@@ -36,13 +36,15 @@ public class GetAllPostCommandResponse
   public string? UserProfilePicture { get; set; }
   public string MediaUrl { get; set; } = null!;
   public string? Caption { get; set; }
+  public DateTime CreatedAt { get; set; }
 
-  public GetAllPostCommandResponse(Guid id, string username, string userProfilePicture, string mediaUrl, string? caption)
+  public GetAllPostCommandResponse(Guid id, string username, string userProfilePicture, string mediaUrl, string? caption, DateTime createdAt)
   {
     Id = id;
     Username = username;
     UserProfilePicture = userProfilePicture;
     MediaUrl = mediaUrl;
     Caption = caption;
+    CreatedAt = createdAt;
   }
 }
