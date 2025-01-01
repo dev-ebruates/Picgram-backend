e3e6c9aaff007d232adf45bccce262e5b27f9c5a
dev-ebruates <devatesebru@gmail.com>
2025-01-01 22:22:43 +0300

    CreatePostCommand response parametre eklendi.

--- Application/Features/Posts/CreatePostCommand.cs
+++ Application/Features/Posts/CreatePostCommand.cs
@@ -21,11 +21,14 @@ public class CreatePostCommand : IRequest<Response<CreatePostCommandResponse>>
       try
       {
         var userId = httpContextAccessor?.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier)
-        ?? throw new Exception("User not found");
-        var post = Post.Create(request.MediaUrl, request.Caption, Guid.Parse(userId));
+          ?? throw new Exception("User not found");
+        var userGuid = Guid.Parse(userId);
+        var user = await unitOfWork.UserRepository.Get(userGuid)
+          ?? throw new Exception("User not found");
+        var post = Post.Create(request.MediaUrl, request.Caption, userGuid);
         unitOfWork.PostRepository.Add(post);
         await unitOfWork.SaveChanges(cancellationToken);
-        return Response<CreatePostCommandResponse>.CreateSuccessResponse(new CreatePostCommandResponse(post.Id, post.UserId, post.MediaUrl, post.Caption), "Post created successfully");
+        return Response<CreatePostCommandResponse>.CreateSuccessResponse(new CreatePostCommandResponse(post.Id, post.UserId, user.Username, user.ProfilePicture, post.MediaUrl, post.Caption, post.CreatedAt), "Post created successfully");
       }
       catch (Exception)
       {
@@ -38,14 +41,20 @@ public class CreatePostCommandResponse
 {
   public Guid Id { get; set; }
   public Guid UserId { get; set; }
+  public string Username { get; set; }
+  public string? UserProfilePicture { get; set; }
   public string MediaUrl { get; set; }
   public string? Caption { get; set; }
+  public DateTime CreatedAt { get; set; }
 
-  public CreatePostCommandResponse(Guid id, Guid userId, string mediaUrl, string? caption)
+  public CreatePostCommandResponse(Guid id, Guid userId, string username, string? userProfilePicture, string mediaUrl, string? caption, DateTime createdAt)
   {
     Id = id;
     UserId = userId;
+    Username = username;
+    UserProfilePicture = userProfilePicture;
     MediaUrl = mediaUrl;
     Caption = caption;
+    CreatedAt = createdAt;
   }
 }
